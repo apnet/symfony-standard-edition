e9155667c67f7e203d2c444c087fc1b3179b9111
Andrey F. Mindubaev <covex.mobile@gmail.com>
2013-12-13 00:47:51 +0400

    PDO session handler

---
+++ app/DoctrineMigrations/Version20131207133000.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Application\Migrations;
+
+use Apnet\Doctrine\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20131207133000 extends AbstractMigration
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function up(Schema $schema)
+  {
+    $this->addSql("CREATE TABLE session (session_id varchar(255) NOT NULL, session_value text NOT NULL, session_time int(11) NOT NULL, PRIMARY KEY (session_id))");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function down(Schema $schema)
+  {
+    $this->addSql("DROP TABLE IF EXISTS session");
+  }
+
+}
