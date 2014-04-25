a6c7c6a099a5571129086c41e1abfc8e77693a25
Andrey F. Mindubaev <covex.mobile@gmail.com>
2014-04-25 22:10:33 +0400

    Switch to doctrine migrations fork

--- app/DoctrineMigrations/Version20130101000000.php
+++ app/DoctrineMigrations/Version20130101000000.php
@@ -2,7 +2,7 @@
 
 namespace Application\Migrations;
 
-use Apnet\Doctrine\AbstractMigration;
+use Doctrine\Bundle\MigrationsBundle\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 class Version20130101000000 extends AbstractMigration
